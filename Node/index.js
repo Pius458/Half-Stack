966f8cfa6c415583b9d52b351e17c4a76ff8324d
pius <piusmusomi@gmail.com>
2024-07-23 12:23:04 +0300

    configured the backend

--- Node/index.js
+++ Node/index.js
@@ -1,10 +1,6 @@
 const express = require('express')
-const cors = require('cors')
-const app = express()
 
-app.use(cors())
-app.use(express.json())
-app.use(express.static('dist'))
+const app = express()
 
 let notes = [
     {
@@ -24,6 +20,25 @@ let notes = [
     }
 
 ]
+app.use(express.static('dist'))
+
+const requestLogger = (request, response, next) => {
+    console.log('Method:', request.method)
+    console.log('Path:  ', request.path)
+    console.log('Body:  ', request.body)
+    console.log('---')
+    next()
+  }
+
+  const cors = require('cors')
+
+  app.use(cors())
+  app.use(express.json())
+  app.use(requestLogger)
+
+  const unknownEndpoint = (request,response) => {
+    response.status(404).send({error: 'unknown endpont'})
+  }
 
 app.get('/',(request,response) => {
     response.send('<h1>Hello World</h1>')
@@ -80,6 +95,8 @@ app.delete('/notes/:id',(request,response)=>{
 
     response.status(204).end()
 })
+
+app.use(unknownEndpoint)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
